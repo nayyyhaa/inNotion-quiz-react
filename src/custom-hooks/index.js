92f67bcbb745150d884c49645ae5219b01dbc44d
nayyyhaa <nehaguptang.ng@gmail.com>
2022-05-13 15:40:38 +0530

    feat: question + result page

---
+++ src/custom-hooks/index.js
@@ -0,0 +1,2 @@
+export { useScrollToTop } from "./useScrollToTop";
+export { usePageViewTracker } from "./usePageViewTracker";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,4 @@
 export { Homepage } from "./Homepage";
+export { RulesPage } from "./RulesPage";
+export { QuestionPage } from "./QuestionPage";
+export { ResultPage } from "./ResultPage";
