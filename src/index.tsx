d425a7d7f95a90e556e6ccd09d66bb4e8836fcf5
nayyyhaa <nehaguptang.ng@gmail.com>
2022-05-18 12:58:52 +0530

    feat: auth with firebase

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { UserProvider, QuizProvider } from "contexts";
+import { UserProvider, QuizProvider, AuthProvider } from "contexts";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <Router>
-      <UserProvider>
-        <QuizProvider>
-          <App />
-        </QuizProvider>
-      </UserProvider>
+      <AuthProvider>
+        <UserProvider>
+          <QuizProvider>
+            <App />
+          </QuizProvider>
+        </UserProvider>
+      </AuthProvider>
     </Router>
   </React.StrictMode>
 );
---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,42 @@
+// import { useAuth } from "contexts/AuthContext";
+// import { useLogin } from "custom-hooks";
+
+import { auth } from "config/firebase-config";
+import { useUser } from "contexts";
+import { signOut } from "firebase/auth";
+
+export const ProfilePage = () => {
+  const {
+    userData: { user },
+  }: any = useUser();
+  // const { logoutHandler } = useLogin();
+  return (
+    <>
+      <main className="profile-content form-content full-wd grid-ctr m-auto p-v-5">
+        <h2 className="title colored-text h1 m-v-2">
+          <span className="circle"></span>
+          Profile
+        </h2>
+        <div className="form-card w-40rm col-flex">
+          <ul className="card left-text no-bullet row-flex text-wrap box-shd w-40rm p-h-2 p-v-4">
+            {/* <li className="w-95p m-b-2">
+              <strong className="m-r-1">First name:</strong>
+              {user?.firstName}
+            </li>
+            <li className="w-95p m-b-2">
+              <strong className="m-r-1">Last name:</strong>
+              {user?.lastName}
+            </li> */}
+            <li className="w-95p m-b-2">
+              <strong className="m-r-1">Email:</strong>
+              {user?.email}
+            </li>
+            <button className="btn primary-btn centered-text w-95p m-h-1" onClick={() => signOut(auth)}>
+              <span>Logout</span>
+            </button>
+          </ul>
+        </div>
+      </main>
+    </>
+  );
+};
